6e18d32d7f992cb4ac259bd0c7e8c299f09cee5d
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-11-17 20:55:01 -0700

    feat: better logging and a new method to test deeper links

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 import { DataService } from './data/data.service';
 
@@ -10,12 +11,22 @@ export class AppController {
   ) {}
 
   @Get()
-  public getHello(@Req() req): Promise<number> {
+  public getHello(@Req() req: Request): Promise<number> {
+    console.log(new Date());
     console.log('Req ip = ', req.ip, req.method, req.originalUrl);
     console.log(JSON.stringify(req.headers));
     return this.dataService.logRequest({
       headers: req.headers,
       source: 'getHello',
     });
+  }
+
+  @Get('/foo')
+  public getFoo(@Req() req): any {
+    console.log(new Date());
+    console.log('Req ip (foo) = ', req.ip);
+    return {
+      foo: 'foobar',
+    };
   }
 }
